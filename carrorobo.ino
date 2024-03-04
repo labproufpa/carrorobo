81e47d6f6799c1368dbd21f0cac48dfed5aded0a
Celson <csnbelem@hotmail.com>
2024-03-04 11:06:21 -0300

    Versão atualizada do código

--- carrorobo.ino
+++ carrorobo.ino
@@ -1,99 +1,123 @@
-#include <Arduino.h>
+#include <Ultrasonic.h>
 
-int motor1Pin2 = 3; // pin 2 on L293D
-int motor1Pin7 = 2; // pin 7 on L293D
-int motor1Pin10 = 4; // pin 10 on L293D
-int motor1Pin15 = 14; // pin 15 on L293D
-int sensorD = 0;
-int sensorE = 0;
-int x = 0;
+Ultrasonic ultrassom(15,16); // define o nome do sensor(ultrassom) onde o trig esta ligado a porta analogica A0 = D14 e o echo porta analogica A1 = D15
+
+#define vai_frente() \
+digitalWrite(EnableE, 1);\
+digitalWrite(EnableD, 1);\
+digitalWrite(MotorE1, 0);\
+digitalWrite(MotorE2, 1);\
+digitalWrite(MotorD1, 0);\
+digitalWrite(MotorD2, 1);\
+
+#define vai_atras() \
+digitalWrite(EnableE, 1);\
+digitalWrite(EnableD, 1);\
+digitalWrite(MotorE1, 1);\
+digitalWrite(MotorE2, 0);\
+digitalWrite(MotorD1, 1);\
+digitalWrite(MotorD2, 0);\
+
+//Referência: Estando atrás do robô
+#define vai_esquerda() \
+digitalWrite(EnableE, 1);\
+digitalWrite(EnableD, 1);\
+digitalWrite(MotorE1, 0);\
+digitalWrite(MotorE2, 1);\
+digitalWrite(MotorD1, 1);\
+digitalWrite(MotorD2, 0);\
+
+//Referência: Estando atrás do robô
+#define vai_direita() \
+digitalWrite(EnableE, 1);\
+digitalWrite(EnableD, 1);\
+digitalWrite(MotorE1, 1);\
+digitalWrite(MotorE2, 0);\
+digitalWrite(MotorD1, 0);\
+digitalWrite(MotorD2, 1);\
+
+int MotorE1 = 6; // Input 1 dos Motores da Esquerda // Pino 2 do L293 e D6 do Arduino Nano
+int MotorE2 = 7; // Input 2 dos Motores da Esquerda // Pino 7 do L293 e D7 do Arduino Nano
+int MotorD1 = 18; // Input 3 dos Motores da Direita // Pino 10 do L293 e A4 do Arduino Nano (A4 do Arduino Nano = D18 do Arduino Nano)
+int MotorD2 = 17; // Input 4 dos Motores da Direita // Pino 15 do L293 e A3 do Arduino Nano (A3 do Arduino Nano = D17 do Arduino Nano)
+int EnableE = 5; // Enable 1 dos motores da esquerda // Pino 1 do L293 e D5 do Arduino Nano
+int EnableD = 9; // Enable 2 dos motores da Direita // Pino 9 do L293 e D9 do Arduino Nano
+int sensorD = 0; // Sensor TCRT5000 da Direita  D9 do Arduino Nano
+int sensorE = 0; //Sensor TCRT5000 da Direita D9 do Arduino Nano
+int contador = 0;
 
 void setup() {
-  pinMode(motor1Pin2, OUTPUT);
-  pinMode(motor1Pin7, OUTPUT);
-  pinMode(motor1Pin10, OUTPUT);
-  pinMode(motor1Pin15, OUTPUT);
-  pinMode(12, INPUT);
-  pinMode(11, INPUT); 
+  pinMode(MotorE1, OUTPUT);
+  pinMode(MotorE2, OUTPUT);
+  pinMode(EnableE, OUTPUT);
+  pinMode(MotorD1, OUTPUT);
+  pinMode(MotorD2, OUTPUT);
+  pinMode(EnableD, OUTPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
   Serial.begin(9600);
 }
 
-void MoveNormal()
+void procura() {
+  vai_frente();
+  delay(1000);
+  vai_esquerda();
+  delay(1000);
+  vai_direita();
+  delay(2000);
+}
+
+void sensorEsquerda() //Teste Sensor de Linha da Esquerda
 {
-  digitalWrite(motor1Pin7, HIGH); // set pin 2 on L293D low
-  digitalWrite(motor1Pin2, LOW); // set pin 7 on L293D high
-  digitalWrite(motor1Pin10, HIGH); // set pin 10 on L293D low
-  digitalWrite(motor1Pin15, LOW); // set pin 15 on L293D high
-  delay(1500);
-  digitalWrite(motor1Pin7, HIGH); // set pin 2 on L293D low
-  digitalWrite(motor1Pin2, LOW); // set pin 7 on L293D high
-  digitalWrite(motor1Pin10, LOW); // set pin 10 on L293D low
-  digitalWrite(motor1Pin15, LOW); // set pin 15 on L293D high
-  delay(1500);
-}
-
-void sensorDireita() //Teste Sensor de Linha da Direita
-{  
-  {
-    sensorD = digitalRead(12);  
-    if (sensorD == 0)  
-    { 
-       digitalWrite(motor1Pin7, LOW); // set pin 2 on L293D low
-       digitalWrite(motor1Pin2, LOW); // set pin 7 on L293D high
-       digitalWrite(motor1Pin10, LOW); // set pin 10 on L293D low
-       digitalWrite(motor1Pin15, LOW); // set pin 15 on L293D high
-       delay(300);
-       digitalWrite(motor1Pin7, LOW); // set pin 2 on L293D low
-       digitalWrite(motor1Pin2, HIGH); // set pin 7 on L293D high
-       digitalWrite(motor1Pin10, LOW); // set pin 10 on L293D low
-       digitalWrite(motor1Pin15, HIGH); // set pin 15 on L293D high
-       delay(1000);
-       digitalWrite(motor1Pin7, LOW); // set pin 2 on L293D low
-       digitalWrite(motor1Pin2, LOW); // set pin 7 on L293D high
-       digitalWrite(motor1Pin10, HIGH); // set pin 10 on L293D low
-       digitalWrite(motor1Pin15, LOW); // set pin 15 on L293D high
-       delay(1000);
-      Serial.println("Linha Detectada na Direita"); 
-    }  
-    else  
-    {  
-      Serial.println("Linha Não Detectada na Direita"); 
-    } 
-  } 
-}
-
-void sensorEsquerda() //Teste Sensor de Linha da Direita
-{  
-  {
-    sensorE = digitalRead(11);  
+    sensorE = digitalRead(2);  
     if (sensorE == 0)  
-    { 
-       digitalWrite(motor1Pin7, LOW); // set pin 2 on L293D low
-       digitalWrite(motor1Pin2, LOW); // set pin 7 on L293D high
-       digitalWrite(motor1Pin10, LOW); // set pin 10 on L293D low
-       digitalWrite(motor1Pin15, LOW); // set pin 15 on L293D high
-       delay(300);
-       digitalWrite(motor1Pin7, LOW); // set pin 2 on L293D low
-       digitalWrite(motor1Pin2, HIGH); // set pin 7 on L293D high
-       digitalWrite(motor1Pin10, LOW); // set pin 10 on L293D low
-       digitalWrite(motor1Pin15, HIGH); // set pin 15 on L293D high
-       delay(1000);
-       digitalWrite(motor1Pin7, HIGH); // set pin 2 on L293D low
-       digitalWrite(motor1Pin2, LOW); // set pin 7 on L293D high
-       digitalWrite(motor1Pin10, LOW); // set pin 10 on L293D low
-       digitalWrite(motor1Pin15, LOW); // set pin 15 on L293D high
-       delay(1000);
-      Serial.println("Linha Detectada na Esquerda"); 
+    {
+       /*vai_atras();
+       delay(1500);
+       vai_direita();
+       delay(2000);*/
+       Serial.println("Linha Detectada na Esquerda");
     }  
     else  
     {  
       Serial.println("Linha Não Detectada na Esquerda"); 
-    } 
-  } 
+    }    
+}
+
+void sensorDireita() //Teste Sensor de Linha da Direita
+{
+    sensorD = digitalRead(3);  
+    if (sensorD == 0)  
+    {
+       /*vai_atras();
+       delay(1500);
+       vai_esquerda();
+       delay(2000);*/
+       Serial.println("Linha Detectada na Direita");
+    }  
+    else  
+    {  
+      Serial.println("Linha Não Detectada na Direita"); 
+    }    
+}
+
+void ataque(){
+  long distancia = ultrassom.Ranging(CM);
+  if (distancia < 10)
+  {
+     vai_frente()
+  }
+  Serial.println("Distancia em cm: ");
+  Serial.println(distancia);
 }
 
 void loop() {
-  MoveNormal();
-  sensorDireita();
-  sensorEsquerda();
+  //vai_frente();
+  //vai_atras();
+  //vai_esquerda();
+  //vai_direita();
+  //procura();
+  //sensorEsquerda();
+  //sensorDireita();
+  ataque();
 }
